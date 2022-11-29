eddd4157870e50d8e046891e1a7b8fbec1ee893c
Cy-2-30 (Dee) <lebulicynthia@gmail.com>
2022-11-29 13:19:39 +0000

    add python  loop -no function

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,12 @@
 console.log("connected");
 
 //my loop//
-for (let i = 0; i <=10; i++){
+for (let i = 0; i <=9; i++){
     let i = "Congratulations!";
     console.log(i);
+ }
+ let myArray = 11;
+ while (myArray <= 20) {
+   console.log(myArray);
+   myArray++;
  }
---
+++ assets/python/script.py
@@ -0,0 +1,7 @@
+print("Are you connected?")
+
+test_string = "iterate over this"
+for letter in test_string:
+    print(letter)
+
+    
